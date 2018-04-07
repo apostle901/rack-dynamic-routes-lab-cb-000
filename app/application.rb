a25863ab8d9f299f7111177fa836584ee8722279
Jawann <jawann@gmail.com>
2018-04-07 04:05:29 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -12,7 +12,7 @@ class Application
         resp.write "#{}"
       end
     else
-      resp.write "Path Not Found"
+      resp.write "Route not found"
       resp.status = 404
     end
     resp.finish
