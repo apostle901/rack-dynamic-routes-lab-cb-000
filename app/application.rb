e878542e698f3d38d057b0a00f56a5b672addeec
Jawann <jawann@gmail.com>
2018-04-07 03:57:19 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,27 @@
+class Application
+
+  @@items = []
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)\
+      item_name = req.path.split("/items/").last
+      if @@items.name.include? (item_name)
+        resp.write "#{}"
+    else
+      resp.write "Path Not Found"
+      resp.status = 404
+    end
+    resp.finish
+  end
+
+  def handle_search(item)
+    if @@items.include?(item)
+      return "#{search_term} is one of our items"
+    else
+      return "Couldn't find #{search_term}"
+    end
+  end
+end
